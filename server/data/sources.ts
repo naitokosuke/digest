95a43ac07b6960e2a6d92fc2c9374a4d1ca3c23c
naitokosuke <kosuke.naito.engineer@gmail.com>
2026-01-12 20:35:31 +0900

    c

--- server/data/sources.ts
+++ server/data/sources.ts
@@ -6,6 +6,12 @@ export const sources: Source[] = [
     name: 'Zenn',
     url: 'https://zenn.dev/kosuke_naito/feed',
     icon: 'simple-icons:zenn'
+  },
+  {
+    type: 'rss',
+    name: 'しずかなインターネット',
+    url: 'https://sizu.me/engineer_naito/rss',
+    icon: 'mdi:pencil'
   }
   // Blog (blog.naito.dev) - RSS will be added later
   // Slides (slides.naito.dev) - manual URLs, no RSS
